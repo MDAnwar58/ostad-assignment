ec98d1327416717682d541cf286c5add8889b3a1
MD Anwar Sayeed <anwar.saeed656@gmail.com>
2023-04-05 18:04:46 +0600

    ostadAssignment9

--- ostadAssignment9/blog/index.php
+++ ostadAssignment9/blog/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog - Title</title>
+    <title>Blog - blog</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
@@ -18,9 +18,16 @@
 
     <!-- ======================blogs start====================== -->
 
-    <div class="grid grid-cols-4 pt-10">
-        <div class="blog-bg col-span-3">
-            <div class="container ps-[5rem]">
+    <div class="grid md:grid-cols-4 grid-cols-1 pt-10">
+        <div class="sidebar px-3 block md:hidden mb-5">
+            <form action="" method="get">
+                <h4 class="text-slate-400 pb-2">Search Blogs</h4>
+                <input type="text" name="search" class="border-4 border-Slate-400/100 w-[100%] ps-3" placeholder="search....">
+                <button type="submit" name="blogSearch" class="bg-neutral-800 text-slate-200 rounded px-3 py-1 mt-1">Search</button>
+            </form>
+        </div>
+        <div class="blog-bg md:col-span-3">
+            <div class="container md:ps-[5rem] mx-auto px-3">
                 <?php
 
                 $blogSql = "SELECT * FROM `blogs`";
--- ostadAssignment9/partials/sidebar.php
+++ ostadAssignment9/partials/sidebar.php
@@ -1,7 +1,9 @@
-<div class="sidebar px-3">
+<div class="sidebar px-3 hidden md:block">
     <form action="" method="get">
         <h4 class="text-slate-400 pb-2">Search Blogs</h4>
         <input type="text" name="search" class="border-4 border-Slate-400/100 w-[100%] ps-3" placeholder="search....">
         <button type="submit" name="blogSearch" class="bg-neutral-800 text-slate-200 rounded px-3 py-1 mt-1">Search</button>
     </form>
 </div>
+
+
